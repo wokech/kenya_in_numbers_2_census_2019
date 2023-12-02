c6df20c04437971ebde004bb64154b4ab04eb62e
William Okech <38021234+wokech@users.noreply.github.com>
2023-12-02 16:31:06 +0300

    Changes to Open def

--- R_scripts/open_defecation.R
+++ R_scripts/open_defecation.R
@@ -7,6 +7,7 @@
 
 #install.packages("sf")
 library(sf) # simple features
+library(sp)
 library(tidyverse)
 library(ggplot2)
 # install.packages("ggrepel")
@@ -14,9 +15,10 @@ library(ggrepel)
 #install.packages("devtools")
 library(devtools)
 #devtools::install_github('bbc/bbplot')
-library(bbplot)
-#devtools::install_github("yutannihilation/ggsflabel")
+#library(bbplot)
+devtools::install_github("yutannihilation/ggsflabel")
 library(ggsflabel)
+devtools::install_github("Shelmith-Kariuki/rKenyaCensus")
 library(rKenyaCensus)
 library(patchwork)
 library(janitor)
@@ -45,7 +47,7 @@ ggplot(kenya_counties_sf) +
 
 # Dangerous and disturbed counties in Kenya
 
-# Remove the "/"
+# Remove the "/" and "-"
 
 kenya_counties_sf$County <- gsub("/", 
                                  " ", 
@@ -83,39 +85,42 @@ df_county$county <- gsub("-",
 p1 <- df_county %>%
   ggplot(aes(x = reorder(county, open_bush), open_bush, fill = open_bush)) + 
   geom_bar(stat = "identity", width = 0.75) + 
-  coord_flip() + 
+  geom_text(aes(label = paste(open_bush), hjust = -0.1), size = 7) +
+  coord_flip(ylim = c(0, 75)) + 
+  labs(x = "County",
+       y = "Percentage of the population (%)",
+       title = "2 out of 3 households in Turkana and Samburu\npractice open defecation",
+       subtitle = "",
+       caption = "Data Source: Kenya Census (2019) | By @afro_dataviz") +
   scale_y_continuous(labels = function(x) format(x, scientific = FALSE)) +
-  scale_fill_distiller(palette = "YlOrRd") +
+  scale_fill_distiller(palette = "Oranges") +
   theme_classic() +
   theme(legend.position="none",
         legend.title = element_blank(),
         panel.grid.major.x=element_blank(),
         panel.grid.minor.x=element_blank(),
         panel.grid.minor.y=element_blank(),
-        axis.text.x = element_text(size = 14),
-        axis.text.y = element_text(size = 10),
-        axis.title.x = element_text(size=16),
-        axis.title.y = element_text(size=16),
-        plot.title = element_text(size = 24, face = "bold"),
-        plot.subtitle = element_text(size = 18),
-        plot.background = element_rect(fill = "azure2", color = "azure2"),
-        panel.background = element_rect(fill = "azure2", color = "azure2")) +
+        axis.title.x = element_text(size = 24, vjust = 1, face = "bold"),
+        axis.title.y = element_text(size = 24, vjust = 1, face = "bold"),
+        axis.text.x = element_text(size = 16, face = "bold", color = "black"),
+        axis.text.y = element_text(size = 16, face = "bold", color = "black"),
+        plot.title = element_text(family="Helvetica", face="bold", size = 36, hjust = 0.5),
+        plot.subtitle = element_text(family="Helvetica", size = 24),
+        plot.title.position = 'plot',
+        plot.caption = element_text(family = "Helvetica",size = 24, face = "bold", hjust = 0.5),
+        plot.caption.position = 'plot',
+        plot.background = element_rect(fill = "azure2", colour = "azure2"),
+        panel.background = element_rect(fill = "azure2", colour = "azure2")) +
   geom_hline(yintercept = 7.4, linetype =1, color = 'grey5', size = .25) +
-  annotate(geom="text", x = 10, y = 8.8, label = "Average (Kenya)" ,size = 4, angle = 90) +
+  annotate(geom="text", x = 40, y = 8.8, label = "Average (Kenya)" ,size = 8, angle = 90) +
   geom_hline(yintercept = 0.8, linetype =2, color = 'grey5', size = .25) +
-  annotate(geom="text", x = 10, y = 2, label = "Average (Urban)" ,size = 4, angle = 90) +
+  annotate(geom="text", x = 40, y = 2, label = "Average (Urban)" ,size = 8, angle = 90) +
   geom_hline(yintercept = 11.5, linetype =2, color = 'grey5', size = .25) +
-  annotate(geom="text", x = 10, y = 13, label = "Average (Rural)" ,size = 4, angle = 90) +
-  labs(title = "",
-       subtitle = "", 
-       x = "County",
-       y = "Percentage (%)",
-       caption = "") +
-  geom_label(label="2 out of 3 households in\nTurkana and Samburu\npractice open defecation", 
-  x=5, y=45, size = 6, label.size = 1, color = "grey5", fill="azure2")
+  annotate(geom="text", x = 40, y = 13, label = "Average (Rural)" ,size = 8, angle = 90)
 
 p1
 
+ggsave("images/human_waste/open_def_bar.png", width = 12, height = 12, dpi = 72)
 
 kenya_open_def_map <- kenya_counties_sf %>%
   left_join(df_county, by = c("County" = "county"))
@@ -123,24 +128,39 @@ kenya_open_def_map <- kenya_counties_sf %>%
 p2 <- kenya_open_def_map %>%
   ggplot(aes(fill = open_bush)) + 
   geom_sf() + 
-  scale_fill_distiller(palette = "YlOrRd") +
+  scale_fill_distiller(palette = "Oranges") +
   theme_void() + 
-  labs(fill = "Percentage (%)",
-       title = "",
+  labs(x = "",
+       y = "",
+       title = "Open defecation is most prevalent in\nthe Northern Counties",
        subtitle = "",
-       caption = "") 
+       caption = "Data Source: Kenya Census (2019) | By @afro_dataviz") +
+  theme(axis.title.x = element_text(size = 28, vjust = 1, face = "bold"),
+        axis.title.y = element_text(size = 28, vjust = 1, face = "bold"),
+        axis.text.x = element_blank(),
+        axis.text.y = element_blank(),
+        plot.title = element_text(family="Helvetica", face="bold", size = 36, hjust = 0.5),
+        plot.subtitle = element_text(family="Helvetica", size = 24),
+        plot.title.position = 'plot',
+        plot.caption = element_text(family = "Helvetica",size = 24, face = "bold", hjust = 0.5),
+        plot.caption.position = 'plot',
+        plot.background = element_rect(fill = "azure2", colour = "azure2"),
+        panel.background = element_rect(fill = "azure2", colour = "azure2"),
+        legend.title = element_blank(),
+        legend.position = "none") 
 
 p2
 
-p1+p2+
-  plot_annotation(title = "Where is Open Defecation Still Widely Practiced in Kenya?",
-                  subtitle = "Percentage of households in every county practicing open defecation",
-                  caption = "Data Source: rKenyaCensus (2019) | By: @willyokech",
-                  theme = theme(plot.title = element_text(family="Helvetica", face="bold", size = 25),
-                                plot.subtitle = element_text(family="Helvetica", face="bold", size = 15),
-                                plot.caption = element_text(family = "Helvetica",size = 12),
-                                plot.background = element_rect(fill = "azure2"))) &
-  theme(text = element_text('Helvetica'))
+ggsave("images/human_waste/open_def_map.png", width = 12, height = 12, dpi = 72)
 
-ggsave("images/human_waste/open_def_1.png", width = 12, height = 8, dpi = 600)
+# Combine the plots together
 
+# p1+p2+
+#   plot_annotation(title = "Where is Open Defecation Still Widely Practiced in Kenya?",
+#                   subtitle = "Percentage of households in every county practicing open defecation",
+#                   caption = "Data Source: rKenyaCensus (2019) | By: @willyokech",
+#                   theme = theme(plot.title = element_text(family="Helvetica", face="bold", size = 25),
+#                                 plot.subtitle = element_text(family="Helvetica", face="bold", size = 15),
+#                                 plot.caption = element_text(family = "Helvetica",size = 12),
+#                                 plot.background = element_rect(fill = "azure2"))) &
+#   theme(text = element_text('Helvetica'))
